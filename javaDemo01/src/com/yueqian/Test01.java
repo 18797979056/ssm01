b0629c829f66909e0c5911e5d0c3e2e450795a8d
xxx <xxx@xx>
2020-11-18 11:37:34 +0800

    idea提交信息

--- javaDemo01/src/com/yueqian/Test01.java
+++ javaDemo01/src/com/yueqian/Test01.java
@@ -3,12 +3,20 @@ package com.yueqian;
 public class Test01 {
 
 	public static void main(String[] args) {
+		test2();
+	}
+
+	public void test2(){
 		for(int i = 1 ; i <= 9 ; i++) {
 			for(int j = 1; j <= i ; j++) {
 				System.out.printf("%sx%s=%-5s",i,j,i*j);
 			}
 			System.out.println();
 		}
+	}
+
+	public void test1{
+		System.out.println("hello world");
 	}
 
 }
