3fb08bf0e218c1dbdaf7828a5b867355fa434009
xxx <xxx@xx>
2020-11-18 11:05:53 +0800

    修改后

--- javaDemo01/src/com/yueqian/Test01.java
+++ javaDemo01/src/com/yueqian/Test01.java
@@ -3,7 +3,12 @@ package com.yueqian;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		for(int i = 1 ; i <= 9 ; i++) {
+			for(int j = 1; j <= i ; j++) {
+				System.out.printf("%sx%s=%-5s",i,j,i*j);
+			}
+			System.out.println();
+		}
 	}
 
 }
